c3fe3b00041c4d7bb3aa4da42fc34c3cff0aa543
franciscogomestv@gmail.com <franciscogomestv@gmail.com>
2021-02-20 10:48:11 +0000

    init commit

---
+++ container/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+  return (
+    <>
+      <h1>Hey</h1>
+      <MarketingApp />
+    </>
+  );
+};
